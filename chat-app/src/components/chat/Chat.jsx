9a07296983a20ba18674aa9f04ef3a99ea69d26e
Arda Gonca <ardagonca14@gmail.com>
2024-07-28 12:47:26 +0300

    1- Chatlist and User Info UI is done...

---
+++ chat-app/src/components/chat/Chat.jsx
@@ -0,0 +1,17 @@
+import "./chat.css"
+
+const Chat = () => {
+    return (
+        <div className="chat">
+            <div className="top">
+                <div className="user"></div>
+                <div className="user"></div>
+
+            </div>
+            <div className="center"></div>
+            <div className="bottom"></div>
+        </div>
+    )
+}
+
+export default Chat
